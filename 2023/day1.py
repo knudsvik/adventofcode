5329f3e103487deec6c5043e280d4a9fb06b1e99
Thorjan Knudsvik <t@knudsvik.com>
2023-12-01 19:43:56 +0100

    day1

---
+++ 2023/day1.py
@@ -0,0 +1,78 @@
+filename = 'day1.txt'
+
+lines = open(filename, 'r').readlines()
+
+sum = 0
+
+for line in lines:
+    nu = ""
+    for char in line:
+        if char.isdigit():
+            nu += char
+            break
+    for char in reversed(line):
+        if char.isdigit():
+            nu += char
+            break
+    sum += int(nu)
+
+print(f"sum part 1: {sum}")
+
+### PART TWO
+
+text_numbers = {"one": 1,
+ "two": 2,
+ "three": 3,
+ "four": 4,
+ "five": 5,
+ "six": 6,
+ "seven": 7,
+ "eight": 8,
+ "nine": 9}
+
+class BreakLoop(Exception):
+    pass
+
+sum = 0
+
+for line in lines:
+    nu = ""
+    front = "unknown"
+    back = "unknown"
+    try:
+        t = ""
+        for char in line:
+            if char.isdigit():
+                front_number = int(char)
+                front = "num"
+                raise BreakLoop
+            else:
+                t += char
+                for key in text_numbers:
+                    if key in t:
+                        front_number = text_numbers[key]
+                        front = "text"
+                        raise BreakLoop
+    except BreakLoop:
+        pass
+    try:
+        t = ""
+        for char in reversed(line.strip()):
+            if char.isdigit():
+                back_number = int(char)
+                back = "num"
+                raise BreakLoop
+            else:
+                t = char + t
+                for key in text_numbers:
+                    if key in t:
+                        back_number = text_numbers[key]
+                        back = "text"
+                        raise BreakLoop
+    except BreakLoop:
+        pass
+    
+    number = int(str(front_number)+str(back_number))
+    sum += number
+
+print(f"sum part 2: {sum}")
