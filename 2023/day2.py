e5893e12fb74eae561ca18c6b5a2a687a0f8b07b
Thorjan Knudsvik <t@knudsvik.com>
2023-12-02 09:28:32 +0100

    part 2

--- 2023/day2.py
+++ 2023/day2.py
@@ -31,3 +31,32 @@ for game in sack:
 
 print(f"The sum of part 1 is {sum}")  # 2061
         
+### PART TWO
+
+game_id  = 0
+sum = 0
+
+for game in sack:
+    game_id += 1
+    #print(f"game: {game}")
+    revealed = game.split(':', 1)[1].strip()
+    
+    red_min, green_min, blue_min = 0, 0, 0
+    
+    for subset in revealed.split(';'):
+        # print(f"  subset: {subset}")
+        red, green, blue = 0, 0, 0
+        
+        for colours in subset.split(','):
+            number, colour = colours.split()
+            number = int(number)
+            
+            if colour == "red" and number > red_min: red_min = number
+            if colour == "green" and number > green_min: green_min = number
+            if colour == "blue" and number > blue_min: blue_min = number
+
+    power = red_min * green_min * blue_min
+    sum += power
+    print(f"Fewest in game {game_id}: {power}")
+
+print(f"The sum of part 2 is {sum}") 
