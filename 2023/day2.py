1b0a1d820e36a0638fd11d8b26928a1cbb2f25d4
Thorjan Knudsvik <t@knudsvik.com>
2023-12-02 09:17:13 +0100

    part 1

---
+++ 2023/day2.py
@@ -0,0 +1,33 @@
+filename = '2023/input/day2.txt'
+sack = open(filename, 'r').readlines()
+
+red_max = 12
+green_max = 13
+blue_max = 14
+
+game_id  = 0
+sum = 0
+
+for game in sack:
+    game_id += 1
+    game_possible = True
+    #print(f"game: {game}")
+    revealed = game.split(':', 1)[1].strip()
+
+    for subset in revealed.split(';'):
+        # print(f"  subset: {subset}")
+        red, green, blue = 0, 0, 0
+        
+        for colours in subset.split(','):
+            number, colour = colours.split()
+            number = int(number)
+            
+            if colour == "red" and number > red_max: game_possible = False
+            if colour == "green" and number > green_max: game_possible = False
+            if colour == "blue" and number > blue_max: game_possible = False
+
+    if game_possible:
+        sum += game_id
+
+print(f"The sum of part 1 is {sum}")  # 2061
+        
