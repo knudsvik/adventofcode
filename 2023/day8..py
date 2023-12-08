1ba1ff35516a7ff55091c41ae3898f63be5afb5e
Thorjan Knudsvik <t@knudsvik.com>
2023-12-08 20:14:24 +0100

    day 8 part 1

---
+++ 2023/day8..py
@@ -0,0 +1,27 @@
+filename = '2023/input/day8.txt'
+lines = open(filename, 'r').readlines()
+
+directions = lines[0].strip('\n')
+nodes = {}
+
+for line in lines[2:]:
+    nodes[line[:3]] = line.split('=')[1].strip()[1:-1].split(', ')
+
+elements = nodes['AAA']
+steps = 0
+node = "someBullshit"
+
+while node != 'ZZZ':
+    for direction in directions:
+        if direction == 'L':
+            node = elements[0]
+        elif direction == 'R':
+            node = elements[1]
+        steps += 1
+        if node == 'ZZZ':
+            break
+        elements = nodes[node]
+        #if steps == len(directions):
+        #    directions += lines[0].strip('\n')
+
+print(steps)
