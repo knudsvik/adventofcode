dc8af16d554417f76ea24e64a92bf2f7c99d550b
Thorjan Knudsvik <t@knudsvik.com>
2023-12-17 20:28:52 +0100

    part two

--- 2023/day16.py
+++ 2023/day16.py
@@ -3,7 +3,7 @@ lines = open(filename, 'r').readlines()
 
 # Coordinate system:
 cs = [list(line.strip()) for line in lines]
-energized = [[False for _ in range(len(lines[0]))] for _ in range(len(lines))]
+energized = [[False for _ in range(len(cs[0]))] for _ in range(len(cs))]
 
 
 def step(x, y, dir):
@@ -74,22 +74,66 @@ for beam in beam_starts:
     dir = beam[2]
     steps = 0  # To hinder loops
 
-    if not (0 <= x < len(lines[0])) or not (0 <= y < len(lines)):
+    if not (0 <= x < len(cs[0])) or not (0 <= y < len(cs)):
         continue
 
     energized[y][x] = True
     
-    while not (x == len(lines[0]) - 1 and y == len(lines) - 1):
+    while not (x == len(cs[0]) - 1 and y == len(cs) - 1):
         x, y, dir = step(x, y, dir)
-        if not (0 <= x < len(cs[0])) or not (0 <= y < len(lines)):
+        if not (0 <= x < len(cs[0])) or not (0 <= y < len(cs)):
             break
         energized[y][x] = True
         steps += 1
         if steps > 1000: # Encountered a loop
             break
+
+
+tiles = sum(cell for row in energized for cell in row)
+print(f"Part one: {tiles}")
+
+# PART TWO
+
+max_energized = 0
+beam_origins = []
+
+# top and bottom
+for i in range(len(cs[0])):
+    beam_origins.append((i, 0, "S"))
+    beam_origins.append((i, len(cs) - 1, "N"))
+# sides
+for i in range(len(cs)):
+    beam_origins.append((0, i, "E"))
+    beam_origins.append((len(cs[0]) - 1, i, "W"))
+
+for origin in beam_origins:
+    energized = [[False for _ in range(len(cs[0]))] for _ in range(len(cs))]
+    beam_starts = [origin]
+
+    for beam in beam_starts:
+        x = beam[0] 
+        y = beam[1]  
+        dir = beam[2]
+        steps = 0  # To hinder loops
+
+        if not (0 <= x < len(cs[0])) or not (0 <= y < len(cs)):
+            continue
+
+        energized[y][x] = True
+        
+        while not (x == len(cs[0]) - 1 and y == len(cs) - 1):
+            if beam == (109, 0, "S"):
+                pass
+            x, y, dir = step(x, y, dir)
+            if not (0 <= x < len(cs[0])) or not (0 <= y < len(cs)):
+                break
+            energized[y][x] = True
+            steps += 1
+            if steps > 1000: # Encountered a loop
+                break
     
-    #print(f"Beam {beam} has come to an end with {steps} steps")
+    tiles = sum(cell for row in energized for cell in row)
+    if tiles > max_energized:
+        max_energized = tiles
 
-
-sum = sum(cell for row in energized for cell in row)
-print(sum)
+print(f"Part two: {max_energized}")
