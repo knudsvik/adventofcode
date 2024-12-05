9fa2b200a93cdf221089cda066f926aa31775a1e
Thorjan Knudsvik <t@knudsvik.com>
2024-12-05 16:46:42 +0100

    remove comments

--- 2024/day5.py
+++ 2024/day5.py
@@ -24,30 +24,21 @@ updates = data[1].split()
 
 result = []
 
-
 for update in updates:
-    #print(f"Update: {update}")
     update = update.split(',')
 
     a = True
     
     for item in range(len(update)):
         while a:
-            #print(f"Checking {update[item]}")
             if int(update[item]) in ruleset:
                 page_rules = ruleset[int(update[item])]
-                #print(f"Page rules: {page_rules}")
                 for other in range(item + 1, len(update)):
-                    #print(f" - Checking this remaining number: {update[other]}")
                     if int(update[other]) in page_rules:
-                        #print("Oh no")
                         a = False
                         break
-                    #else:
-                        #print("all good")  
             break   
     if a == True:
-        result.append(int(update[len(update) // 2]))  # If not breaking rules 
-        #print("- - Result added") 
-    
+        result.append(int(update[len(update) // 2]))  
+
 print(f"Result: {sum(result)}")
